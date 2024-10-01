bcfdea0b1de7794af426b3aaeda7c20d6894fb0d
dmamanipar <dmamanipar@unsa.edu.pe>
2024-10-01 12:03:53 -0500

    proyecto almacen

---
+++ SysAlmacenFX/src/main/java/pe/edu/upeu/sysalmacenfx/SysAlmacenFxApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.sysalmacenfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysAlmacenFxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysAlmacenFxApplication.class, args);
+	}
+
+}
